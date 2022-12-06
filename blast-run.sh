d70fc4d089dbc4cf9a6abdee73e35f33e38124cc
Manoj Kumar <sekhwal@gmail.com>
2022-12-06 11:07:46 -0500

    Update blast-run.sh

--- blast-run.sh
+++ blast-run.sh
@@ -1,4 +1,4 @@
 for f in *.fasta*
 do
-    /home/mmk6053/ncbi-blast-2.12.0/bin/blastn -query $f -db plasmid_db -out ${f/fasta*/results} -outfmt "6 qseqid sseqid length qlen slen pident evalue sstart send"
+    /home/ncbi-blast-2.12.0/bin/blastn -query $f -db plasmid_db -out ${f/fasta*/results} -outfmt "6 qseqid sseqid length qlen slen pident evalue sstart send"
 done
